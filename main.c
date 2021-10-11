94c1b7802bfd70df1fe19d802671eae641d55882
Jefferson Lima <jeffyuri@hotmail.com>
2021-10-11 14:57:09 -0300

    Adição de header do capitulo 3 e separacao do capitulo em um outro módulo diferente do main()

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "cap3.h"
 
 int main()
 {
-    int x, y, z;
-    printf("Digite um número: ");
-    scanf("%d", &x);
-    printf("%d\n", x);
-    y = x - 1;
-    z = x + 1;
-    printf("O antecessor desse número é %d e seu sucessor é %d.", y, z);
-    return 0;
+    atv1();
 }
